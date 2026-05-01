9316e7a4535cb7fbdf80aec2c07b56d2ec9e6e01
justkel <kelyniwuh@gmail.com>
2026-05-01 13:58:45 -0700

    route api to create lab result

---
+++ src/app/api/lab-result/create/route.ts
@@ -0,0 +1,59 @@
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import { print } from 'graphql';
+
+import {
+  CreateLabResultDocument,
+  CreateLabResultMutation,
+  CreateLabResultMutationVariables,
+} from '@/shared/graphql/generated/graphql';
+
+import {
+  GraphQLErrorShape,
+  handleGraphQLError,
+} from '@/lib/handle-graphql-error';
+
+const GATEWAY_URL = process.env.NEXT_PUBLIC_GATEWAY_URL!;
+
+export async function POST(req: Request) {
+  const accessToken = (await cookies()).get('access_token')?.value;
+
+  if (!accessToken) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  const body = await req.json();
+
+  const variables: CreateLabResultMutationVariables = {
+    data: body,
+  };
+
+  const res = await fetch(GATEWAY_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({
+      query: print(CreateLabResultDocument),
+      variables,
+    }),
+  });
+
+  const json: {
+    data?: CreateLabResultMutation;
+    errors?: GraphQLErrorShape[];
+  } = await res.json();
+
+  const errorResponse = handleGraphQLError(json.errors);
+  if (errorResponse) return errorResponse;
+
+  if (!json.data?.createLabResult) {
+    return NextResponse.json(
+      { error: 'Failed to create lab result' },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json(json.data.createLabResult);
+}
